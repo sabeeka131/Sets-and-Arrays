618acabade94cf61571055af81abffbc91c92565
Aliza <59551300+sabeeka131@users.noreply.github.com>
2025-02-07 19:02:26 +0500

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,14 @@
+Set1A = {'blue', 'green'}
+Set2A = {'blue', 'yellow'}
+
+
+sym_diff_A = Set1A.symmetric_difference(Set2A)
+print("Symmetric difference for Set1A and Set2A:", sym_diff_A)
+
+
+Set1B = {1, 2, 3, 4, 5}
+Set2B = {1, 5, 6, 7, 8, 9}
+
+
+sym_diff_B = Set1B.symmetric_difference(Set2B)
+print("Symmetric difference for Set1B and Set2B:", sym_diff_B)
